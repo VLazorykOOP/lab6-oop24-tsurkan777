a11e0ce4530ae46e11a5f5e12e1c87a9b18e4b96
tsurkan777 <tsurkan.sofiia@chnu.edu.ua>
2025-05-13 06:43:32 +0300

    Update Tsurkan3.cpp

--- Tsurkan3.cpp
+++ Tsurkan3.cpp
@@ -1,80 +1,48 @@
 #include <iostream>
-#include <cmath> // для sqrt()
-
+#include <string>
 using namespace std;
 
-// Абстрактний клас Function
-class Function {
+// Клас працівник
+class Employee {
 public:
-    // Віртуальна функція обчислення y за x
-    virtual double calculate(double x) = 0; // чисто віртуальна
-    virtual ~Function() {} // віртуальний деструктор
-};
+    string position;
+    double salary;
 
-// Клас пряма: y = ax + b
-class Line : public Function {
-private:
-    double a, b;
-public:
-    Line(double a_, double b_) : a(a_), b(b_) {}
+    Employee(string pos, double sal) : position(pos), salary(sal) {}
 
-    double calculate(double x) override {
-        return a * x + b;
+    void showEmployee() {
+        cout << "Посада: " << position << ", Зарплата: " << salary << endl;
     }
 };
 
-// Клас еліпс: (x^2)/a^2 + (y^2)/b^2 = 1 => y = ±sqrt(b^2*(1 - x^2/a^2))
-class Ellipse : public Function {
-private:
-    double a, b;
+// Клас батько-сімейства
+class Father {
 public:
-    Ellipse(double a_, double b_) : a(a_), b(b_) {}
+    int numberOfChildren;
 
-    double calculate(double x) override {
-        if (abs(x) > a) {
-            cout << "x поза межами еліпса. ";
-            return NAN; // не число
-        }
-        return b * sqrt(1 - (x * x) / (a * a)); // тільки верхня половина
+    Father(int children) : numberOfChildren(children) {}
+
+    void showFather() {
+        cout << "Кількість дітей: " << numberOfChildren << endl;
     }
 };
 
-// Клас гіпербола: (x^2)/a^2 - (y^2)/b^2 = 1 => y = ±sqrt((x^2/a^2 - 1) * b^2)
-class Hyperbola : public Function {
-private:
-    double a, b;
+// Клас працівник-батько
+class EmployeeFather : public Employee, public Father {
 public:
-    Hyperbola(double a_, double b_) : a(a_), b(b_) {}
+    EmployeeFather(string pos, double sal, int children)
+        : Employee(pos, sal), Father(children) {}
 
-    double calculate(double x) override {
-        if (abs(x) < a) {
-            cout << "x у забороненій зоні (|x| < a). ";
-            return NAN;
-        }
-        return b * sqrt((x * x) / (a * a) - 1); // тільки верхня гілка
+    void showAll() {
+        showEmployee();
+        showFather();
     }
 };
 
 int main() {
     system("chcp 65001");
-    Function* f1 = new Line(2.0, 3.0);        // y = 2x + 3
-    Function* f2 = new Ellipse(5.0, 3.0);     // еліпс з a=5, b=3
-    Function* f3 = new Hyperbola(2.0, 4.0);   // гіпербола з a=2, b=4
-
-    double x;
-
-    cout << "Введіть x: ";
-    cin >> x;
-
-    cout << "Пряма: y = " << f1->calculate(x) << endl;
-    cout << "Еліпс: y = " << f2->calculate(x) << endl;
-    cout << "Гіпербола: y = " << f3->calculate(x) << endl;
-
-    // Звільнення пам’яті
-    delete f1;
-    delete f2;
-    delete f3;
+    EmployeeFather ef("Інженер", 15000.0, 3);
+    ef.showAll();
 
     return 0;
 }
-
